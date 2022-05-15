d456dbec365d95f9731362514251442f1e85cd65
root <root@ip-172-31-25-33.ec2.internal>
2022-05-15 08:17:40 +0000

    Completed the polls app with features to vote and display the results

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'django-insecure-%%x!5p03)3%=!a-i)x-gxxzmidk(z7)r_vsl#t)gs@j(0accr+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Dubai'
 
 USE_I18N = True
 
